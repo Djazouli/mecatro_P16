4c22884294a86b167cf51b0d7daf6be4d62f81e2
16lemoing <guillaume.le_moing@mines-paristech.fr>
2017-12-12 17:07:45 +0100

    LieuCle

---
+++ Robot_P16/Robot_P16/Map/LieuCle.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.SPOT;
+using Robot_P16.Map.Surface;
+
+namespace Robot_P16.Map
+{
+    class LieuCle
+    {
+        private ElementSurface surfaceDeControle;
+        private PointOriente pointDeReference;
+        private double toleranceAngulaire;
+
+        public LieuCle(ElementSurface surface, PointOriente point, double tolerance)
+        {
+            surfaceDeControle = surface;
+            pointDeReference = point;
+            toleranceAngulaire = tolerance;
+        }
+
+        suisJeAuBonEndroit
+    }
+}
