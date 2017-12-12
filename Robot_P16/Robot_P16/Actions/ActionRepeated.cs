55e5093921980d7b13431c7db7d74314bebb78de
16pierre <quentin.pierre@mines-paristech.fr>
2017-12-12 16:46:43 +0100

    FIx

---
+++ Robot_P16/Robot_P16/Actions/ActionRepeated.cs
@@ -0,0 +1,9 @@
+using System;
+using Microsoft.SPOT;
+
+namespace Robot_P16.Actions
+{
+    class ActionRepeated
+    {
+    }
+}
---
+++ Robot_P16/Robot_P16/Robot/GestionnaireAction.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using Microsoft.SPOT;
+
+namespace Robot_P16.Robot
+{
+    class GestionnaireAction
+    {
+
+        private static Hashtable ACTION_PER_TYPE = new Hashtable();
+
+        public static void loadActions()
+        {
+            ACTION_PER_TYPE.Clear();
+        }
+
+        private static void loadAction
+
+
+    }
+}
