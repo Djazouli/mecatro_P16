4c4bdfa37eeeea41126d3c307f402687c2bb5fac
16pierre <quentin.pierre@mines-paristech.fr>
2017-12-12 16:04:08 +0100

    Ajout lieux clés

---
+++ Robot_P16/Robot_P16/Map/Lieux.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using Microsoft.SPOT;
+using Robot_P16.Robot;
+
+namespace Robot_P16.Map
+{
+    /// <summary>
+    /// TypeDeLieu est une enum qui permet de récupérer un lieu clé en donnant le TypeDeLieu et la couleur de l'équipe.
+    /// </summary>
+    public enum TypeDeLieu
+    {
+        POINT_DEPART_GR,
+        POINT_DEPART_PR,
+
+        CUBES_1,
+        CUBES_2, // ...
+
+        INTERRUPTEUR,
+        BASSIN_RECUPERATEUR; //...
+    }
+    class LieuCle
+    {
+        ElementSurface surfaceDeControle;
+        PointOriente pointDeReference;
+        double toleranceAngulaire;
+        
+        private static Hashtable LIEUX_CLES = new Hashtable();
+
+        private static void addLieuCle(TypeDeLieu type, CouleurEquipe couleur, LieuCle lieu) {
+            if(!LIEUX_CLES.Contains(couleur)) {
+                LIEUX_CLES.Add(couleur, new Hashtable());
+            }
+            ((Hashtable)LIEUX_CLES[couleur]).Add(type, lieu);
+        }
+
+        public static void LoadAllLieuxCles() {
+            LIEUX_CLES.Clear();
+            
+            // AJOUT DES LIEUX CLES ICI
+
+            // FIN DE LAJOUT DES LIEUX CLES
+            
+        }
+
+        public Boolean IsAtTheRightPlace(PointOriente pt) {
+            return false;
+        }
+        public static LieuCle GetLieuCleFor(TypeDeLieu type, CouleurEquipe couleur) {
+            if(LIEUX_CLES.Contains(couleur)) {
+                Hashtable lieux_associes = (Hashtable)LIEUX_CLES[couleur];
+                if(lieux_associes.Contains(type))
+                    return (LieuCle)lieux_associes[type];
+            }
+            return null;
+        }
+
+    }
+}
