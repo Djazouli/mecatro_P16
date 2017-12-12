9462223d38baa8056231e33d205080994b31af44
16pierre <quentin.pierre@mines-paristech.fr>
2017-12-12 17:11:36 +0100

    GestionnaireAction

--- Robot_P16/Robot_P16/Robot/GestionnaireAction.cs
+++ Robot_P16/Robot_P16/Robot/GestionnaireAction.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using Microsoft.SPOT;
+using Robot_P16.Actions;
 
 namespace Robot_P16.Robot
 {
@@ -12,9 +13,25 @@ namespace Robot_P16.Robot
         public static void loadActions()
         {
             ACTION_PER_TYPE.Clear();
+            loadActionHomologation();
         }
 
-        private static void loadAction
+        private static void loadActionHomologation()
+        {
+            // Blablabla
+        }
+
+        private static void setMotherAction(ModeOperatoire mode, Action a)
+        {
+            if (ACTION_PER_TYPE.Contains(mode))
+            {
+                ACTION_PER_TYPE[mode] = a;
+            }
+            else
+            {
+                ACTION_PER_TYPE.Add(mode, a);
+            }
+        }
 
 
     }
