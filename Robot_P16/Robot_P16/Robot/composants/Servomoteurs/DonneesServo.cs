59c01652192e9fdb9d33e3e53084d9311fbc7632
mariegirod <32104155+mariegirod@users.noreply.github.com>
2018-03-06 17:52:35 +0100

    Update DonneesServo.cs

--- Robot_P16/Robot_P16/Robot/composants/Servomoteurs/DonneesServo.cs
+++ Robot_P16/Robot_P16/Robot/composants/Servomoteurs/DonneesServo.cs
@@ -24,8 +24,8 @@ namespace Robot_P16.Robot.Composants.Servomoteurs
  	 public const float ANGLE_PR_ASCENSEURGAUCHE_MONTERUNITE = 342.86; //pour monter de 60mm, monter un cube d'une hauteur de 60mm
  	 public const float ANGLE_PR_ASCENSEURGAUCHE_DESCENDREUNITE = -342.86;
  	 
- 	 public const float ANGLE_PR_ASCENSEURDROIT_MONTERUNITE = 195.58; //valeur inconnue et différente de l'aute ascenseur car systèmes physiques différents
- 	 public const float ANGLE_PR_ASCENSEURDROIT_DESCENDREUNITE = -195.58;
+ 	 public const float ANGLE_PR_ASCENSEURDROIT_MONTERUNITE = 202.32; //pour monter de 60mm, systeme different de l'autre ascenseur
+ 	 public const float ANGLE_PR_ASCENSEURDROIT_DESCENDREUNITE = -202.32;
  	 
  	 public const float ANGLE_PR_ROTATIONGAUCHE_HORAIRE = 90;//stockage d'un cube sur la pile (90 ou -90)
  	 public const float ANGLE_PR_ROTATIONGAUCHE_ANTIHORAIRE = -90;
