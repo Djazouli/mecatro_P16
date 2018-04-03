74b35132249716b2e82e311c81eef8128d74edbc
falcird <matthieu.cornet@live.fr>
2018-04-03 17:40:42 +0200

    verbosité IHM

--- Robot_P16/Robot_P16/Robot/composants/IHM/Parametrization.cs
+++ Robot_P16/Robot_P16/Robot/composants/IHM/Parametrization.cs
@@ -26,20 +26,24 @@ namespace Robot_P16.Robot.composants.IHM
             this.typeRobot = typeRobot;
             this.modeOperatoire = modeOperatoire;
             this.couleurEquipe = couleurEquipe;
+            Informations.printInformations(Priority.LOW, "Robot.composants.IHM.Parametrization.CustomParametrization : le type de robot, le mode opératoire et la couleur de l'équipe ont été paramétrés");
         }
 
         public override TypeRobot GetTypeRobot()
         {
+            Informations.printInformations(Priority.LOW, "Robot.composants.IHM.Parametrization.TypeRobot : information sur le type de robot récupérée" );
             return typeRobot;
         }
 
         public override ModeOperatoire GetModeOperatoire()
         {
+            Informations.printInformations(Priority.LOW, "Robot.composants.IHM.Parametrization.ModeOperatoire : information sur le mode opératoire récupérée");
             return modeOperatoire;
         }
 
         public override CouleurEquipe GetCouleurEquipe()
         {
+            Informations.printInformations(Priority.LOW, "Robot.composants.IHM.Parametrization.CouleurEquipe : information sur la couleur de l'équipe récupérée");
             return couleurEquipe;
         }
     }
